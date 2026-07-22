aa8fff701ae6401bf128bbbbfc727719b6ad4b2c
MandrakeMaster <gabelphilip@hotmail.fr>
2026-07-22 22:54:43 +0200

    feat: ajout du header dynamique avec les catégories de l'API et correction du logo

---
+++ site/src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>Bienvenue sur Trouve ton Artisan</h1>
+      {/* Le reste de ton contenu */}
+    </main>
+  );
+}
